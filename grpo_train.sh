b00d4af37990cc1b5334150cfc09a9fd0caf8242
Asad Shahab <116843180+Asad-Shahab@users.noreply.github.com>
2025-08-09 02:09:30 -0400

    Update grpo_train.sh

--- grpo_train.sh
+++ grpo_train.sh
@@ -24,4 +24,18 @@ source /scratch/ashahab4/miniforge3/bin/activate
 conda activate logic
 
 # Run training
-./launch_training.sh 4 --sft_model_path sft_models/final --use_mixed_precision
+./launch_training.sh 4 \
+    --sft_model_path sft_models/final \
+    --grpo_batch_size 2 \
+    --grpo_gradient_accumulation_steps 2 \
+    --grpo_learning_rate 3e-6 \
+    --num_generations 8 \
+    --temperature 1.2 \
+    --max_seq_length 3000 \
+    --lora_rank 64 \
+    --use_mixed_precision \
+    --grpo_max_steps 6000 \
+    --grpo_logging_steps 50 \
+    --grpo_save_steps 500 \
+    --grpo_output_dir ./grpo_models_4gpu_option3 \
+    --grpo_dataset dataset/proverqa_simplified.json
