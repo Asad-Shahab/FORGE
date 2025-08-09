e9ce5389dd9cf0c6a09139b5b27ce64f4b0bf470
Asad Shahab <116843180+Asad-Shahab@users.noreply.github.com>
2025-08-09 02:10:41 -0400

    Update grpo_train.sh

--- grpo_train.sh
+++ grpo_train.sh
@@ -30,12 +30,11 @@ conda activate logic
     --grpo_gradient_accumulation_steps 2 \
     --grpo_learning_rate 3e-6 \
     --num_generations 8 \
-    --temperature 1.2 \
+    --temperature 1 \
     --max_seq_length 3000 \
     --lora_rank 64 \
     --use_mixed_precision \
     --grpo_max_steps 6000 \
     --grpo_logging_steps 50 \
     --grpo_save_steps 500 \
-    --grpo_output_dir ./grpo_models_4gpu_option3 \
-    --grpo_dataset dataset/proverqa_simplified.json
+    --grpo_output_dir ./grpo_models_4gpu
